02a5613b5ababd3e245e033b370127779b8fc9a2
Dhruv Jain <djain@dhruvs-mbp.attlocal.net>
2017-06-12 22:55:53 -0400

    initial commit

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
     }
 
     override func didReceiveMemoryWarning() {
